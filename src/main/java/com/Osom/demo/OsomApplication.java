45924d76d3a0f6100a3528f961dffc59acce243f
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-03-27 21:47:12 +0530

    Initial commit

---
+++ src/main/java/com/Osom/demo/OsomApplication.java
@@ -0,0 +1,13 @@
+package com.Osom.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Osom/demo/OsomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Osom.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
